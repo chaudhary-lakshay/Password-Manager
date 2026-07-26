7bdacbb9edc7f9b96199f3018b662fd3ebf72d2e
hgshreyas <hgshreyas.cs24@bmsce.ac.in>
2026-07-26 21:54:48 +0530

    Add list saved sites menu option

--- src/PasswordManager/PasswordManager.java
+++ src/PasswordManager/PasswordManager.java
@@ -61,7 +61,8 @@ public class PasswordManager {
             System.out.println("2. Retrieve Password");
             System.out.println("3. Load Vault File");
             System.out.println("4. Save to New Vault File");
-            System.out.println("5. Exit");
+            System.out.println("5. List Saved Sites");
+            System.out.println("6. Exit");
             System.out.print("Choose an option: ");
             int choice=0;
 
@@ -71,7 +72,7 @@ public class PasswordManager {
             } 
             catch (NumberFormatException e) 
             {
-                System.out.println("Invalid input! Please enter a valid option number (1-5).\n");
+                System.out.println("Invalid input! Please enter a valid option number (1-6).\n");
                 continue; // Restarts the loop to show the menu again
             }
 
@@ -149,6 +150,9 @@ public class PasswordManager {
                     }
                     break;
                 case 5:
+                    manager.listSavedSites();
+                    break;
+                case 6:
                     System.exit(0);
                     break;
                 default:
@@ -183,6 +187,15 @@ public class PasswordManager {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public void listSavedSites() {
+        if (passwordStore.isEmpty()) {
+            System.out.println("No sites saved yet.");
+        } else {
+            System.out.println("Saved sites:");
+            passwordStore.keySet().stream().sorted().forEach(site -> System.out.println("  " + site));
         }
     }
 
